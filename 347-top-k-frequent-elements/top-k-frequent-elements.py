0ad56b2d63ff30e9b28b0970e95d8f0b22c54e3b
Sameer Jain <sameerjain501@gmail.com>
2026-03-03 06:16:22 -0500

    Time: 6 ms (56.75%) | Memory: 22.7 MB (86.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,24 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
-        build a frequency map of elements 
-        create ans list 
-        iterate thru map k times, removing the max each time 
-        '''
+
         freq_map = {}
         for i in range(len(nums)):
-            freq_map[nums[i]] = freq_map.get(nums[i],0) + 1
-        
+            freq_map[nums[i]] = freq_map.get(nums[i], 0) + 1
+
+        freq_map = sorted(freq_map.items(), key=lambda x: x[1],reverse=False)
+
         ans = []
-        while k > 0:
-            max_val = 0
-            max_key = 0
-            for key,value in freq_map.items():
-                if value > max_val:
-                    max_key = key
-                    max_val = value
-            ans.append(max_key)
-            del freq_map[max_key]
-            k -=1
+        while len(ans) < k:
+            ans.append(freq_map.pop()[0])
 
         return ans
