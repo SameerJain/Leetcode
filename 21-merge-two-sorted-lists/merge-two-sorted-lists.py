82d4d065defcc1ae20e8d23df5ecd6af71ecaa70
Sameer Jain <sameerjain501@gmail.com>
2026-03-05 13:35:31 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,73 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-"""
-Curr1
-1  -> 2   -> 4
-
-
-
-Curr2
-2->   3   ->  4
-
-1->1->2->3->4->4
-
-
-iterate thru both lists
-
-if one value is less
-    set that as the next node
-    set temp.next = curr
-
-if one list hasnt been passed thru
-    add everything thats left
-"""
-
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return None
-        if list1 is None:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 and not list2:
+            return None 
+        if not list1:
             return list2
-        if list2 is None:
+        if not list2:
             return list1
-            
-        curr1 = list1
-        curr2 = list2
-
-        new_head = None
-
-        if curr1.val < curr2.val:
-            new_head = curr1
-            curr1 = curr1.next
+    
+        if list1.val < list2.val:
+            temp = list1
+            list1 = list1.next 
         else:
-            new_head = curr2
-            curr2 = curr2.next
+            temp = list2
+            list2 = list2.next
 
-        temp = new_head
+        temp_head = temp 
 
-        while curr1 and curr2:
-            if curr1.val <= curr2.val:
-                temp.next = curr1
-                curr1 = curr1.next
-            elif curr2.val < curr1.val:
-                temp.next = curr2
-                curr2 = curr2.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next 
+            else:
+                temp.next = list2
+                list2 = list2.next
             temp = temp.next
         
-        while curr1:
-            temp.next = curr1
-            curr1 = curr1.next
-            temp = temp.next
-        
-        while curr2:
-            temp.next = curr2
-            curr2 = curr2.next
-            temp = temp.next
-        
-        return new_head
-        
+        if list1:
+            temp.next = list1
+        if list2:
+            temp.next = list2
+        return temp_head
 
-
+# @lc code=end
 
         
